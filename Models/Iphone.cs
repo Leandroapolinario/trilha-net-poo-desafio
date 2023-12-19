778d07d7127796312902b8428b15e2e0ff1420da
Leandro Apolinário Leite <leandro.ap.leite@gmail.com>
2023-12-19 15:56:54 -0300

    Desafio concluído

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,20 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    // Correção: Herdar da classe "Smartphone"
+    public class Iphone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        // Correção: Passar os parâmetros do construtor para as propriedades
+        public Iphone(string numero, string modelo, string imei, int memoria)
+            : base(numero, modelo, imei, memoria)
+        {
+            // TODO: Implementar lógica específica do construtor, se necessário
+        }
+
+        // Correção: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            // TODO: Implementar lógica de instalação de aplicativo para iPhone
+            Console.WriteLine($"Instalando {nomeApp} no iPhone...");
+        }
     }
 }
--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,20 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    // Correção: Herdar da classe "Smartphone"
+    public class Nokia : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        // Correção: Passar os parâmetros do construtor para as propriedades
+        public Nokia(string numero, string modelo, string imei, int memoria)
+            : base(numero, modelo, imei, memoria)
+        {
+            // TODO: Implementar lógica específica do construtor, se necessário
+        }
+
+        // Correção: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            // TODO: Implementar lógica de instalação de aplicativo para Nokia
+            Console.WriteLine($"Instalando {nomeApp} no Nokia...");
+        }
     }
 }
